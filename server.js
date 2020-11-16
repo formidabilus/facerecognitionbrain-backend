78df6b963b7aecda4fb9fcd1bde6565792330584
formidabilus <chiriac_razvan@ymail.com>
2020-11-16 23:51:23 +0200

    changing response

--- server.js
+++ server.js
@@ -24,36 +24,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: "123",
-      name: "John",
-      password: "cookies",
-      email: "john@gmail.com",
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: "124",
-      name: "Sally",
-      password: "bananas",
-      email: "sally@gmail.com",
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-  login: [
-    {
-      id: "987",
-      has: "",
-      email: "john@gmail.com",
-    },
-  ],
-};
-
 app.get("/", (req, res) => {
-  res.send(database.users);
+  res.send("it is working!");
 });
 
 app.post("/signin", signin.handleSignin(db, bcrypt));
