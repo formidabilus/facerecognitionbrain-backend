730e4183ac3f1434e187e9218e61b8ab3f57ec34
formidabilus <chiriac_razvan@ymail.com>
2020-11-17 00:36:40 +0200

    added database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-round-07122",
-    user: "vnm",
-    password: "parola123",
-    database: "smart-brain",
+    host: "process.env.DATABASE_URL",
+    ssl: true,
   },
 });
 
