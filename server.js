9bae2b62b463fef8f0ba601080c2cd9150a2fd41
formidabilus <chiriac_razvan@ymail.com>
2020-11-17 00:30:39 +0200

    added host for postgress in heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-round-07122",
     user: "vnm",
     password: "parola123",
     database: "smart-brain",
