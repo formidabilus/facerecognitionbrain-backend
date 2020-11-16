62885a0117f061988d0246265b06409184b26132
formidabilus <chiriac_razvan@ymail.com>
2020-11-16 23:24:12 +0200

    adding port

--- server.js
+++ server.js
@@ -74,8 +74,8 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen((port = 3000), () => {
-  console.log(`app is runnning on port ${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is runnning on port ${process.env.PORT}`);
 });
 
 /*
